8c37fbac9022379a63b4e2ceb7fc91d8edcb6779
yztz <781089956@qq.com>
2021-07-09 19:16:19 +0800

    del test

--- test/antlrTest.java
+++ test/antlrTest.java
@@ -4,13 +4,13 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
 
-public class antlrTest {
-    @Test
-    public void test() {
-        CharStream input = CharStreams.fromString("12 * 2 + 12\r\n");
-        SysYLexer lexer = new SysYLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SysYParser parser = new SysYParser(tokens);
-        ParseTree tree = parser.prog();
-    }
-}
+//public class antlrTest {
+//    @Test
+//    public void test() {
+//        CharStream input = CharStreams.fromString("12 * 2 + 12\r\n");
+//        SysYLexer lexer = new SysYLexer(input);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        SysYParser parser = new SysYParser(tokens);
+//        ParseTree tree = parser.prog();
+//    }
+//}
