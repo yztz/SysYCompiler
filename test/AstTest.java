4bc8343f5b5b0b80b0230796673508ef0d643954
yztz <781089956@qq.com>
2021-08-05 23:40:17 +0800

    增加入口函数

--- test/AstTest.java
+++ test/AstTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 public class AstTest {
     AstVisitor visitor = new AstVisitor();
 
-    @Test
+
     public void testExp() {
         SysYParser parser = Utils.getParser("test/testExp.sys");
         ParseTree tree = parser.exp();
         AstNode root = visitor.visit(tree);
 //        Utils.interpreterAst(root);
     }
-    @Test
+
     public void testFull() {
         SysYParser parser = Utils.getParser("test/testFull.sys");
         ParseTree tree = parser.compUnit();
