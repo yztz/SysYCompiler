428ac0808ab15095ecee4279625b37f51237206f
yztz <781089956@qq.com>
2021-08-11 20:01:20 +0800

    push

--- src/Compiler.java
+++ src/Compiler.java
@@ -6,7 +6,7 @@ import ast.AstVisitor;
 import common.Utils;
 import ir.IRParser;
 
-import org.antlr.v4.gui.TreeViewer;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -52,26 +52,26 @@ public class Compiler {
         codeGenerator.genCode();
     }
 
-    public static void makeVisible(Parser parser, Tree tree) {
-        //show AST in GUI
-        JFrame frame = new JFrame("Antlr AST");
-        JPanel panel = new JPanel();
-        TreeViewer viewer = new TreeViewer(Arrays.asList(
-                parser.getRuleNames()),tree);
-//        viewer.setScale(0.8); // Scale a little
-        panel.add(viewer);
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        while(frame.isVisible()) {
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    public static void makeVisible(Parser parser, Tree tree) {
+//        //show AST in GUI
+//        JFrame frame = new JFrame("Antlr AST");
+//        JPanel panel = new JPanel();
+//        TreeViewer viewer = new TreeViewer(Arrays.asList(
+//                parser.getRuleNames()),tree);
+////        viewer.setScale(0.8); // Scale a little
+//        panel.add(viewer);
+//        frame.add(panel);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//        while(frame.isVisible()) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     public static SysYParser getParser(String fileName) {
         CharStream input = null;
